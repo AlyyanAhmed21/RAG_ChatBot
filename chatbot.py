f075414fc0a28c32b500033a9ae0f0239b266f9e
AlyyanAhmed21 <alyyanawan19@gmail.com>
2025-11-20 15:38:00 +0500

    Fix langchain import and dependencies for cloud

--- chatbot.py
+++ chatbot.py
@@ -3,7 +3,7 @@ import time
 from dotenv import load_dotenv
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
 from langchain_groq import ChatGroq
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
